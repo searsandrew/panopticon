a24f30929740676522b895cf66d4493e9708dc5f
Andrew Sears <andrew@mayfifteenth.com>
2026-05-29 07:45:02 -0500

    feat: update auth layout with background image and add new image asset

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,7 +4,7 @@
     <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
         <div class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div class="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-e dark:border-neutral-800">
-                <div class="absolute inset-0 bg-neutral-900"></div>
+                <div class="absolute inset-0 bg-[url(/img/pexels-bradanton-12450216.jpg)] bg-cover bg-neutral-900"></div>
                 <a href="{{ route('dashboard') }}" class="relative z-20 flex items-center text-lg font-medium" wire:navigate>
                             <span class="flex h-10 w-10 items-center justify-center rounded-md">
                                 <x-app-logo-icon class="me-2 h-7 fill-current text-white" />
